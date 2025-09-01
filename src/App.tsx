169335b94068dfa9aee998732b71e9a483f0aa93
JingpuRen <yy5396681@163.com>
2025-09-02 01:47:32 +0800

    引入各组件并添加样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss'
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
+import PageInfoList from './components/Page';
+import { useState } from "react";
+import { QuestionCircleOutlined, SyncOutlined } from '@ant-design/icons';
+import { FloatButton } from 'antd';
 
 function App() {
+  const [currentPageId, setCurrentPageId] = useState<number>(1)
+
+  const handleClickDown = () => {
+    let newPageId = currentPageId + 1
+    setCurrentPageId(newPageId)
+  }
+
+  const handleClickUp = () => {
+    let newPageId = currentPageId - 1
+    setCurrentPageId(newPageId)
+  }
+
+  const getRenderComponent = (pageId: number) => {
+    const matchedItem = PageInfoList.find((item) => item.page_id === pageId);
+    return matchedItem ? <matchedItem.component /> : null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+
+      <div className={styles.header}>
+        <Tooltip title="上一页">
+          <Button shape="circle" icon={<UpOutlined />} onClick={handleClickUp} disabled={currentPageId === 1 ? true : false} />
+        </Tooltip>
+      </div>
+
+      <div className={styles.middle}>
+        {getRenderComponent(currentPageId)}
+      </div>
+
+      <div className={styles.footer}>
+        <Tooltip title="下一页">
+          <Button shape="circle" icon={<DownOutlined />} onClick={handleClickDown} disabled={currentPageId === PageInfoList.length ? true : false} />
+        </Tooltip>
+      </div>
     </div>
   );
 }
